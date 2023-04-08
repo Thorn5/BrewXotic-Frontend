001b790e30e434046fd55c21855903eebc483555
Trygve Horn <trygvehorn@gmail.com>
2023-04-08 23:02:32 +0200

    Basic Routing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
 import "./App.css";
 import { Route, Routes, Switch, NavLink, Link } from "react-router-dom";
+import Navbar from "./elements/Navbar";
+import ShopFront from "./components/ShopFront"
+import ProductDetail from "./components/ProductDetail"
+import ShoppingCart from "./components/ShoppingCart"
+import CheckOut from "./components/CheckOut"
+import Confirmation from "./components/Confirmation"
+
 function App() {
   return (
     <div className="App">
---
+++ src/elements/Navbar.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { Outlet, Link, NavLink } from "react-router-dom";
+
+export default function Navbar() {
+  return (
+    <div>
+      <nav>
+        <ul>
+          <li>
+            <NavLink to="/"> ShopFront </NavLink>
+          </li>
+          <li>
+            <NavLink to="/ProductDetail"> ProductDetail </NavLink>
+          </li>
+          <li>
+            <NavLink to="/ShoppingCart"> ShoppingCart </NavLink>
+          </li>
+          <li>
+            <NavLink to="/CheckOut"> CheckOut </NavLink>
+          </li>
+          <li>
+            <NavLink to="/Confirmation"> Confirmation </NavLink>
+          </li>
+        </ul>
+      </nav>
+      <Outlet />
+    </div>
+  );
+}
