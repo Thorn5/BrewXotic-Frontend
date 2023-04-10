3f66577b3cb82ddcfa3580650c0f2100851ed39e
Trygve Horn <trygvehorn@gmail.com>
2023-04-10 11:02:20 +0200

    formatted

--- src/App.js
+++ src/App.js
@@ -25,94 +25,94 @@ import "./App.css";
 
 // export default App;
 
-import React from 'react';
-import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 function Home() {
-return (
-<div>
-<h1>Home</h1>
-<p>Welcome to our website!</p>
-</div>
-);
+  return (
+    <div>
+      <h1>Home</h1>
+      <p>Welcome to our website!</p>
+    </div>
+  );
 }
 
 function Products() {
-return (
-<div>
-<h1>Products</h1>
-<p>Check out our products!</p>
-</div>
-);
+  return (
+    <div>
+      <h1>Products</h1>
+      <p>Check out our products!</p>
+    </div>
+  );
 }
 
 function About() {
-return (
-<div>
-<h1>About Us</h1>
-<p>Learn more about us!</p>
-</div>
-);
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Learn more about us!</p>
+    </div>
+  );
 }
 
 function App() {
-return (
-<Router>
-<div className="App">
-<nav>
-<ul>
-<li>
-<Link to="/">Home</Link>
-</li>
-<li>
-<Link to="/products">Products</Link>
-</li>
-<li>
-<Link to="/about">About Us</Link>
-</li>
-</ul>
-</nav>
+  return (
+    <Router>
+      <div className="App">
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/products">Products</Link>
+            </li>
+            <li>
+              <Link to="/about">About Us</Link>
+            </li>
+          </ul>
+        </nav>
 
-<main>
-<Route exact path="/" component={Home} />
-<Route path="/products" component={Products} />
-<Route path="/about" component={About} />
-</main>
+        <main>
+          <Route exact path="/" component={Home} />
+          <Route path="/products" component={Products} />
+          <Route path="/about" component={About} />
+        </main>
 
-<style jsx>{`
-.App {
-display: flex;
-}
+        <style jsx>{`
+          .App {
+            display: flex;
+          }
 
-nav {
-width: 200px;
-height: 100vh;
-background-color: #f0f0f0;
-}
+          nav {
+            width: 200px;
+            height: 100vh;
+            background-color: #f0f0f0;
+          }
 
-ul {
-list-style-type: none;
-margin: 0;
-padding: 0;
-}
+          ul {
+            list-style-type: none;
+            margin: 0;
+            padding: 0;
+          }
 
-li {
-padding: 10px;
-cursor: pointer;
-}
+          li {
+            padding: 10px;
+            cursor: pointer;
+          }
 
-li.active {
-background-color: #ddd;
-}
+          li.active {
+            background-color: #ddd;
+          }
 
-main {
-flex-grow: 1;
-padding: 20px;
-}
-`}</style>
-</div>
-</Router>
-);
+          main {
+            flex-grow: 1;
+            padding: 20px;
+          }
+        `}</style>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
