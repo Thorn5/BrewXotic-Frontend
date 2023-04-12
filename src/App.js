58478adb3c0c32f18dfe8b20ad1b1b7fcf228810
Trygve Horn <trygvehorn@gmail.com>
2023-04-12 22:46:51 +0200

    ss

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
-import Navbar from "./elements/Navbar";
+import Grid from "./elements/Grid";
+// import Navbar from "./elements/Navbar";
 import ShopFront from "./components/ShopFront"
 import ProductDetail from "./components/ProductDetail"
 import ShoppingCart from "./components/ShoppingCart"
@@ -10,15 +11,17 @@ import Confirmation from "./components/Confirmation"
 function App() {
   return (
     <div className="App">
+      <Grid> {/* Navbar.js incorporated in Grid */}
       <Routes>
-        <Route path="/" element={<Navbar />}>
-          <Route index element={<ShopFront />} />
+        <Route path="/" element={<ShopFront />}>
+          {/* <Route index element={<ShopFront />} /> */}
           <Route path="/ProductDetail" element={<ProductDetail />} />
           <Route path="/ShoppingCart" element={<ShoppingCart />} />
           <Route path="/CheckOut" element={<CheckOut />} />
           <Route path="/Confirmation" element={<Confirmation />} />
         </Route>
       </Routes>
+      </Grid>
     </div>
   );
 }
---
+++ src/elements/Grid.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from './Navbar';
+import './Grid.css';
+
+const Grid = ({ children }) => {
+  return (
+    <div className="grid-container">
+      <Navbar />
+      <div className="content">{children}</div>
+    </div>
+  );
+};
+
+export default Grid;
