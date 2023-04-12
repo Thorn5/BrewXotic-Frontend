bad2f0d9986c555c648fc69cc0d348cb254c6bf8
Trygve Horn <trygvehorn@gmail.com>
2023-04-12 22:18:49 +0200

    typo

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
-import "./App.css";
 // src/App.js
+import "./App.css";
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Grid from "./components/Grid";
