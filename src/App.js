a258d9af6e05977afa22d2a5e49c0c705675753d
Trygve Horn <trygvehorn@gmail.com>
2023-04-12 21:19:08 +0200

    fixed

--- src/App.js
+++ src/App.js
@@ -1,21 +1,23 @@
 import "./App.css";
 // src/App.js
-import React from 'react';
-import { BrowserRouter as Route, Routes } from 'react-router-dom';
-import Grid from './components/Grid';
-import Home from './pages/Home';
-import About from './pages/About';
-import Contact from './pages/Contact';
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Grid from "./components/Grid";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
 
 const App = () => {
   return (
-      <Grid>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/contact" element={<Contact />} />
-      </Routes>
-      </Grid>
+    <>
+        <Grid>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </Grid>
+    </>
   );
 };
 
