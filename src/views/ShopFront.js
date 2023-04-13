820d0a4d9a4dcd5e97a231e77eb21ffeb1a109cf
Trygve Horn <trygvehorn@gmail.com>
2023-04-13 09:10:54 +0200

    ss

--- src/views/ShopFront.js
+++ src/views/ShopFront.js
@@ -2,9 +2,10 @@ import React from 'react'
 import useAsyncAwait from "../customHooks/useAsyncAwait"
 
 export default function ShopFront() {
+  const url = "http://localhost:5050/api/products"
   const { loading, error, apiData } = useAsyncAwait(url);
   return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })}
-    <ul>{path.map((item, index) => (<li key={index}>{item}</li>))}</ul>
+    {/* <ul>{path.map((item, index) => (<li key={index}>{item}</li>))}</ul> */}
   </div>)}</>)
 }
 
