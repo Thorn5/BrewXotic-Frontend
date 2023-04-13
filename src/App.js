013502ceff60397673ea78cc3cab0dbf44d2faf9
Trygve Horn <trygvehorn@gmail.com>
2023-04-13 08:57:07 +0200

    Switched to views

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Grid from "./elements/Grid";
-import ShopFront from "./components/ShopFront";
-import ProductDetail from "./components/ProductDetail";
-import ShoppingCart from "./components/ShoppingCart";
-import CheckOut from "./components/CheckOut";
-import Confirmation from "./components/Confirmation";
+import ShopFront from "./views/ShopFront";
+import ProductDetail from "./views/ProductDetail";
+import ShoppingCart from "./views/ShoppingCart";
+import CheckOut from "./views/CheckOut";
+import Confirmation from "./views/Confirmation";
 
 function App() {
   return (
