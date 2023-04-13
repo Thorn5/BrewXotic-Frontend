fbf5adc3beae6bdfeafb0d4850bfded345d5a2cf
Trygve Horn <trygvehorn@gmail.com>
2023-04-13 09:24:03 +0200

    Added useFetch

---
+++ src/customHooks/useFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+  const [apiData, setApiData] = useState(null);
+
+  // console.log(url);
+  // console.log(data);
+
+  const fetchData = () => {
+    if (typeof url !== "string" || url.length === 0) {
+      throw new Error("Invalid URL");}
+    setLoading(true)
+    fetch(url)
+      .then((response) => response.json())
+      .then((data) => setApiData(data))
+      .catch((err) => {
+        setError(err);
+      })
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+  
+  useEffect(() => {
+        fetchData();
+  }, [url]);
+
+  return { loading, error, apiData };
+};
+
+export default useFetch;
+
+// Bring to parent component:
+// const apiKey = "12345";
+// const requestParams = `?api_key=${apiKey}`;
+// const baseUrl = "https://whatever";
+// const requestEndpoint = "/end/point/"
+// const url = `${baseUrl}${requestEndpoint}${requestParams}`;
+
+// const { loading, error, apiData } = useFetch(url);
+
+// return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({apiData})}</div>)}</>)
+// map: <ul>{path.map((item, index) => (<li key={index}>{item}</li>))}</ul>
--- src/views/ShopFront.js
+++ src/views/ShopFront.js
@@ -1,9 +1,11 @@
 import React from 'react'
-import useAsyncAwait from "../customHooks/useAsyncAwait"
+// import useAsyncAwait from "../customHooks/useAsyncAwait"
+import useFetch from "../customHooks/useFetch"
 
 export default function ShopFront() {
   const url = "http://localhost:5050/api/products"
-  const { loading, error, apiData } = useAsyncAwait(url);
+  // const { loading, error, apiData } = useAsyncAwait(url);
+  const { loading, error, apiData } = useFetch(url);
   return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })}
     {/* <ul>{path.map((item, index) => (<li key={index}>{item}</li>))}</ul> */}
   </div>)}</>)
