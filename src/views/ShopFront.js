9e72881751a690d7bde6ec7f5c25b105043ff5dd
Trygve Horn <trygvehorn@gmail.com>
2023-04-13 11:48:56 +0200

    Fixed useMockApiData.js

--- src/views/ShopFront.js
+++ src/views/ShopFront.js
@@ -1,14 +1,19 @@
 import React from 'react'
 // import useAsyncAwait from "../customHooks/useAsyncAwait"
-import mockApiData from '../customHooks/mockApiData';
+import mockApiData from '../customHooks/useMockApiData';
 
 export default function ShopFront() {
-  // const url = "http://localhost:5050/api/products"
+  // const apiKey = "";
+  // const requestParams = ``;
+  // const baseUrl = "https://api.publicapis.org";
+  // const requestEndpoint = "/entries"
+  // const url = `${baseUrl}${requestEndpoint}${requestParams}`;
   // const { loading, error, apiData } = useAsyncAwait(url);
-  
+  const { loading, error, apiData } = mockApiData();
+
   return (<>
-  {/* {loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })} </div>)} */}
-  <div>ShopFront</div>
+    {loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })} </div>)}
+    <div>ShopFront</div>
   </>)
 }
 
