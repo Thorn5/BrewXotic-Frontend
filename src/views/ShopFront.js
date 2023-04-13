8ab09a900a10d9fee4ae0f73b75d8c5c0f16cf7f
Trygve Horn <trygvehorn@gmail.com>
2023-04-13 11:12:54 +0200

    ss

--- src/views/ShopFront.js
+++ src/views/ShopFront.js
@@ -1,12 +1,13 @@
 import React from 'react'
-import useAsyncAwait from "../customHooks/useAsyncAwait"
-// import useFetch from "../customHooks/useFetch"
+// import useAsyncAwait from "../customHooks/useAsyncAwait"
+import mockApiData from '../customHooks/mockApiData';
 
 export default function ShopFront() {
-  const url = "http://localhost:5050/api/products"
-  const { loading, error, apiData } = useAsyncAwait(url);
-  // const { loading, error, apiData } = useFetch(url);
-  return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })} </div>)}</>)
-  // return ( <div>ShopFront</div> )
+  // const url = "http://localhost:5050/api/products"
+  // const { loading, error, apiData } = useAsyncAwait(url);
+  
+  return (<>{loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData })} </div>)}
+  <div>ShopFront</div>
+  </>)
 }
 
