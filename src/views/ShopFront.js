7559663b316c5b3dd1fcfdef8d8976cdd1464b0e
Trygve Horn <trygvehorn@gmail.com>
2023-04-15 09:35:28 +0200

    mapped products

--- src/views/ShopFront.js
+++ src/views/ShopFront.js
@@ -1,18 +1,25 @@
 import React from 'react'
-// import useAsyncAwait from "../customHooks/useAsyncAwait"
 import mockApiData from '../customHooks/useMockApiData';
-
 export default function ShopFront() {
-  // const baseUrl = "https://api.publicapis.org";
-  // const requestEndpoint = "/entries"
-  // const requestParams = ``;
-  // const url = `${baseUrl}${requestEndpoint}${requestParams}`;
-  // const { loading, error, apiData, moduleCalled } = useAsyncAwait(url);
   const { loading, error, apiData, moduleCalled } = mockApiData();
-
-  return (<>
-    {loading ? (<p>Loading...</p>) : error ? (`${error}`) : (<div>{apiData && console.log({ apiData, moduleCalled })} </div>)}
-    <div>ShopFront</div>
-  </>)
+  return (
+    <>
+      <div>ShopFront</div>
+      {loading ? (
+        <p>Loading...</p>
+      ) : error ? (`${error}`) : (
+        <div className="grid-container">
+          {apiData &&
+            apiData.map(item => (
+              <div key={item._id} className="grid-item" id={item._id} style={{ backgroundColor: 'lightblue', borderRadius: '7px', boxShadow: '2px 2px 5px 0px rgba(0,0,0,0.2)', width: '13rem' }}>
+                <div><img src={item.images.thumbnail} alt={item.name} /></div>
+                <div>Name: {item.name}</div>
+                <div>Price: €{item.price['$numberDecimal']}</div>
+                <div>Quantity Available: {item.quantity_available}</div>
+              </div>
+            ))}
+        </div>
+      )}
+    </>
+  );
 }
-
